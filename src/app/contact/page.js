ee35f111d0eba981b1abdeadb3e8fcee0661b977
Bijoy <bijoymogor@gmail.com>
2024-03-06 01:21:39 +0530

    Solved Hydation Problem

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,20 +1,27 @@
 "use client"
 import Image from "next/image";
 import styles from "./contact.module.css";
+import { useEffect, useState } from "react";
 
 const ContactPage = () => {
     console.log("bijoy");       //serverside rendering if not paired with "use client"
     
+    const [isClient,setIsClient] = useState(false);
     /**
      * Causes hydartion problem as the item generated in server side is different than client side
      */
     let a = Math.random();
-    console.log(a); 
+    // console.log(a); 
+
+    //hydartion problems are solved by using Hooks like use State and rendering variable inside useEffect
     
-    
+    useEffect(()=>{
+        setIsClient(true);
+    },[])
+
     return (
         <div className={styles.container}>
-            {a}    
+            {isClient && a}    
             {/* Error: Text content does not match server-rendered HTML. */}
             <div className={styles.imgContainer}>
                 <Image src="/contact.png" alt="" fill className={styles.img} />
