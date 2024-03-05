1e5d20e771f5bd20c5e614e7db237c459487aada
Bijoy <bijoymogor@gmail.com>
2024-03-06 01:07:24 +0530

    Hydartion Problem

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,8 +4,18 @@ import styles from "./contact.module.css";
 
 const ContactPage = () => {
     console.log("bijoy");       //serverside rendering if not paired with "use client"
+    
+    /**
+     * Causes hydartion problem as the item generated in server side is different than client side
+     */
+    let a = Math.random();
+    console.log(a); 
+    
+    
     return (
         <div className={styles.container}>
+            {a}    
+            {/* Error: Text content does not match server-rendered HTML. */}
             <div className={styles.imgContainer}>
                 <Image src="/contact.png" alt="" fill className={styles.img} />
             </div>
